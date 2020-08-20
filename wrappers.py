684c00c59cca5452da6dbe905a7ab4ad3d4db478
Kaito32768 <suzukikaito14.ne.jp@gmail.com>
2020-08-21 00:30:49 +0900

    add gymwrapper for pybullet

--- wrappers.py
+++ wrappers.py
@@ -3,7 +3,49 @@ import numpy as np
 from viewer import OpenCVImageViewer
 
 
-class GymWrapper(object):
+class GymWrapper_PyBullet(object):
+    """
+    Gym interface wrapper for pybullet env
+    """
+    metadata = {'render.modes': ['human', 'rgb_array']}
+    reward_range = (-np.inf, np.inf)
+
+    def __init__(self, env, cam_dist=3, cam_yaw=0, cam_pitch=-30, render_width=320, render_height=240):
+        self._env = env
+        self._env.env._cam_dist = cam_dist
+        self._env.env._cam_yaw = cam_yaw
+        self._env.env._cam_pitch = cam_pitch
+        self._env.env._render_width = render_width
+        self._env.env._render_height = render_height
+
+    def __getattr(self, name):
+        return getattr(self._env, name)
+
+    @property
+    def observation_space(self):
+        width = self._env.env._render_width
+        height = self._env.env._render_height
+        return gym.spaces.Box(0, 255, (height, width, 3), dtype=np.uint8)
+
+    @property
+    def action_space(self):
+        return self._env.action_space
+
+    def step(self, action):
+        _, reward, done, info = self._env.step(action)
+        obs = self._env.render(mode="rgb_array")
+        return obs, reward, done, info
+
+    def reset(self):
+        self._env.reset()
+        obs = self._env.render(mode="rgb_array")
+        return obs
+
+    def render(self, mode='human', **kwargs):
+        return self._env.render(mode, **kwargs)
+ 
+
+class GymWrapper_DMC(object):
     """
     Gym interface wrapper for dm_control env wrapped by pixels.Wrapper
     """
